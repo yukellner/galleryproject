6060a5808339d1a5ba3bdae9431b5a37c2f88e41
Yuval Kellner <yukellner@gmail.com>
2022-04-04 18:30:35 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 console.log('Starting up');
+
+function init(){
+    renderprojects()
+}
+
+
+function renderprojects(){
+    var projs = getProjects()
+    console.log('projs',projs)
+
+
+    var curstr = projs.map( proj => 
+
+        "<div class='col-md-4 col-sm-6 portfolio-item'>"+
+        "<a class='portfolio-link' data-toggle='modal' href='#portfolioModal1'>"+
+            "<div class='portfolio-hover'>"+
+              "<div class='portfolio-hover-content'>"+
+                "<i class='fa fa-plus fa-3x'></i>"+
+              "</div>"+
+            "</div>"+
+            "<img class='img-fluid' src='img/portfolio/"+proj.name+"-thumbnail.jpg' alt=''>"+
+          "</a>"+
+          "<div class='portfolio-caption'>"+
+            "<h4>"+proj.name+"</h4>"+
+            "<p class='text-muted'>Illustration</p>"+
+          "</div>"+
+        "</div>"
+        
+        
+        
+        
+        
+        
+        
+    )
+
+    
+
+    $('.portfolio-grid').html(curstr)
+}
---
+++ proj-service.js
@@ -0,0 +1,41 @@
+var gProjs = []
+
+function getProjects() {
+    _getProjects()
+    return gProjs
+}
+
+
+function _getProjects() {
+
+    gProjs = [
+        {
+            id: "Minesweeper",
+            name: "Minesweeper",
+            title: "Better push those boxes",
+            desc: "lorem i  psum lorem ipsum lorem ipsum",
+            url: "projs/sokoban",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"],
+        },
+        {
+            id: "In-Picture Game",
+            name: "In-Picture Game",
+            title: "Better push those boxes",
+            desc: "lorem i  psum lorem ipsum lorem ipsum",
+            url: "projs/sokoban",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"],
+        },
+        {
+            id: "Touch Nums",
+            name: "Touch Nums",
+            title: "Better push those boxes",
+            desc: "lorem i  psum lorem ipsum lorem ipsum",
+            url: "projs/sokoban",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"],
+        },
+        
+    ]
+}
